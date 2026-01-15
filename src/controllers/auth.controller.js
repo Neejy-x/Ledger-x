475f5b6a556656a87c3b0aeb56ab634dec108540
neejy-x <tatsmymail@gmail.com>
2026-01-15 12:09:03 +0100

    added loginhandler to controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -22,3 +22,25 @@ export const signupHandler = async (req, res) => {
     accessToken,
   });
 };
+
+export const loginHandler = async(req, res) => {
+    const payload = req.body
+
+    const { user, refreshToken, accessToken} = await AuthService.login(payload)
+
+    res.cookie('jwt', refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    })
+
+    res.status(200).json({
+        status: 'Success',
+        message: 'login Successful',
+        user: {
+            name: user.full_name,
+            email: user.email
+        },
+        accessToken
+    })
+}
