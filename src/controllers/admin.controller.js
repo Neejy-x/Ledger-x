c8b366a11c0f10eb2d8c85966a4d69d0e123a817
neejy-x <tatsmymail@gmail.com>
2026-01-19 08:25:09 +0100

    admin rouutes and user status

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,9 +1,27 @@
+const { use } = require("react")
+const { updateUserRole } = require("../service/admin.service")
 
 
-const getUsersHandler = async (req, res) => {
+exports.getUsersHandler = async (req, res) => {
     const users = await getUsers()
     res.status(200).json({
         status: 'Successful',
         users
+    })
+}
+
+
+exports.updateUserRoleHandler = async (req, res) => {
+    const {userId} = req.params
+    const {role} = req.body
+    const user = await updateUserRole({userId, role})
+    res.status(200).json({
+        status: 'Successful',
+        user: {
+            id: user.id,
+            fullName: user.fullName,
+            email: user.email,
+            role: user.role
+        }
     })
 }
--- src/service/admin.service.js
+++ src/service/admin.service.js
@@ -1,5 +1,18 @@
-const {Users } = require('../database/models')
+const {User } = require('../database/models')
 
-const getUsers = async() => {
-    return await Users.findAll({ attributes: { include: ['id', 'fullName', 'email']}});
+ exports.getUsers = async() => {
+    return await User.findAll({ attributes: { include: ['id', 'fullName', 'email']}});
+}
+
+exports.updateUserRole = async({userId, role}) => {
+    const user = await User.findByPk(userId)
+    if(!user){
+        const e = new Error('User not found')
+        e.statusCode = 404
+        throw e
+    }
+
+    user.role = role
+    await user.save()
+    return user
 }
